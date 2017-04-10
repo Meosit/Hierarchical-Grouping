766c6efeade9ee5bf01ad321578076ad509aa326
Mikhail Snitavets <mksn13@gmail.com>
2017-04-11 00:06:56 +0300

    Fix unnecessary pow call

--- src/main/kotlin/by/mksn/miapr/HierarchicalGrouping.kt
+++ src/main/kotlin/by/mksn/miapr/HierarchicalGrouping.kt
@@ -1,7 +1,7 @@
 package by.mksn.miapr
 
 fun <T> combinations(arr: Array<T>): List<Pair<T, T>> {
-    val numCombinations = Math.pow(arr.size.toDouble(), 2.0).toInt()
+    val numCombinations = arr.size * arr.size
     val list = mutableListOf<Pair<T, T>>()
     for (i in 0..numCombinations - 1) {
         val first = arr[i / arr.size % arr.size]
